72a94d2c5433da17a1ae3c77c44800c956d6c7da
Ravindu Chathuranga <ravicdasanayaka@gmail.com>
2023-09-24 21:13:10 +0530

    added basics

--- SignIn.js
+++ SignIn.js
@@ -1,22 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { View, SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
 
-function SignIn(){
-    const ui=(
-        <SafeAreaView style={styles.container}>
-            <StatusBar hidden={true}/>
+export function SignIn() {
+    const ui = (
+        <SafeAreaView style={styles.contaier}>
+            <StatusBar hidden={true} />
+            <View style={styles.view1}>
+                <Text>Mobile</Text>
+                <TextInput style={styles.textInput} />
+            </View>
+            <View style={styles.view1}>
+                <Text>Password</Text>
+                <TextInput style={styles.textInput} />
+            </View>
         </SafeAreaView>
     );
-    return ui();
+    return ui;
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
 
-    contaier:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"white"
+    contaier: {
+        flex: 1,
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor: "white"
     },
+    view1: {
+        width: '100%',
+        padding: 20,
+    },
+    text1:{
+        fontSize:16,
+        fontWeight:'bold',
+    },
+    textInput: {
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginTop: 5,
+        fontSize: 20,
+        padding: 8,
+        width: "100%",
+        borderRadius:8,
+    },
+
 
 });
